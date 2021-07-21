099c796029aab431757281ab10022a8dea5bcdf2
George Petrakis <georgepetrakis99@gmail.com>
2021-07-21 09:25:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DMARC_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
